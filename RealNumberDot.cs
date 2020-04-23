d216a12899e670deecc2973cf3ff651896440e54
alips96 <alips96@gmail.com>
2020-04-23 11:02:24 +0200

    real number dot state

--- RealNumberDot.cs
+++ RealNumberDot.cs
@@ -14,9 +14,71 @@ namespace TokenizerProject
             mainForm = _mainForm;
         }
 
+        public void ToInitialState()
+        {
+            mainForm.currentState = mainForm.initialState;
+        }
+
+        private void ToRealNumberDotDigitState()
+        {
+            mainForm.currentState = mainForm.realNumberDotDigitState;
+        }
+
         public void UpdateState()
         {
-            throw new NotImplementedException();
+            CheckForInput();
+        }
+
+        private void CheckForInput()
+        {
+            if (mainForm.plainTextQueue.Count > 0)
+            {
+                int currentChar = mainForm.plainTextQueue.Dequeue(); //finds the first element of queue.
+
+                if (currentChar >= 48 && currentChar <= 57) //if it is a digit
+                {
+                    ExtendCurrentToken();
+                    ToRealNumberDotDigitState();
+                }
+                else
+                {
+                    ExtendCurrentToken();
+                    WriteCurrentTokenInList();
+                    ToInitialState();
+                }
+            }
+            else
+            {
+                if (mainForm.currentToken != null)
+                {
+                    WriteCurrentTokenInList();
+                }
+
+                mainForm.isFinished = true; // Stops the Main While of program.
+            }
+        }
+
+        private void WriteCurrentTokenInList()
+        {
+            mainForm.tokensList.Add(new Token(mainForm.currentToken, "Unknown Token"));
+            mainForm.currentToken = null;
+        }
+
+        private void ExtendCurrentToken()
+        {
+            int ascii = Convert.ToInt32(mainForm.tokensLinkedList.First());
+
+            if (!(ascii > 0 && ascii <= 32))
+            {
+                mainForm.currentToken += mainForm.tokensLinkedList.First();
+            }
+
+            mainForm.tokensLinkedList.RemoveFirst();
+
+            //if (mainForm.tokensLinkedList.Count == 0)
+            //{
+            //    WriteCurrentTokenInList();
+            //}
         }
     }
 }
