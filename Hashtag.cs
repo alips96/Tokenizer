dac0d2c9583421f50123ec0ee8ece02105a5de8c
alips96 <alips96@gmail.com>
2020-04-23 11:35:07 +0200

    hashtag state

--- Hashtag.cs
+++ Hashtag.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace TokenizerProject
 {
-    public class Hashtag : IState
+    public class Hashtag : IGetBackToInitialState
     {
         MainForm mainForm;
         public Hashtag(MainForm _mainForm)
@@ -14,9 +14,69 @@ namespace TokenizerProject
             mainForm = _mainForm;
         }
 
+        public void ToInitialState()
+        {
+            mainForm.currentState = mainForm.initialState;
+        }
+
         public void UpdateState()
         {
-            throw new NotImplementedException();
+            CheckForInput();
+        }
+
+        private void CheckForInput()
+        {
+            if (mainForm.plainTextQueue.Count > 0)
+            {
+                int currentChar = mainForm.plainTextQueue.Last(); //finds the first element of queue.
+
+                if (currentChar >= 65 && currentChar <= 90 || //if it is an english character.
+                   (currentChar >= 97 && currentChar <= 122))
+                {
+                    //mainForm.plainTextQueue.Dequeue();
+                    //ExtendCurrentToken();
+                    //ToDigitDotState();
+                    mainForm.plainTextQueue.Dequeue();
+                    ExtendCurrentToken();
+                }
+                else
+                {
+                    WriteCurrentTokenInList();
+                    ToInitialState();
+                }
+            }
+            else
+            {
+                if (mainForm.currentToken != null)
+                {
+                    WriteCurrentTokenInList();
+                }
+
+                mainForm.isFinished = true; // Stops the Main While of program.
+            }
+        }
+
+        private void WriteCurrentTokenInList()
+        {
+            mainForm.tokensList.Add(new Token(mainForm.currentToken, "Hashtag Token"));
+            mainForm.currentToken = null;
+        }
+
+        private void ExtendCurrentToken()
+        {
+            int ascii = Convert.ToInt32(mainForm.tokensLinkedList.First());
+
+            if (!(ascii > 0 && ascii <= 32))
+            {
+                mainForm.currentToken += mainForm.tokensLinkedList.First();
+            }
+
+            mainForm.tokensLinkedList.RemoveFirst();
+
+            //if (mainForm.tokensLinkedList.Count == 0)
+            //{
+            //    WriteCurrentTokenInList();
+            //}
         }
     }
 }
