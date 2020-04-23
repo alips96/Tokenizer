d9e6edde20020db5a891599fd7969d6bd3bbd31a
alips96 <alips96@gmail.com>
2020-04-23 11:51:03 +0200

    special token state

--- SpecialToken.cs
+++ SpecialToken.cs
@@ -14,9 +14,70 @@ namespace TokenizerProject
             mainForm = _mainForm;
         }
 
+        public void ToInitialState()
+        {
+            mainForm.currentState = mainForm.initialState;
+        }
+
         public void UpdateState()
         {
-            throw new NotImplementedException();
+            WriteCurrentTokenInList();
+            ToInitialState();
         }
+
+        //private void CheckForInput()
+        //{
+        //    if (mainForm.plainTextQueue.Count > 0)
+        //    {
+        //        int currentChar = mainForm.plainTextQueue.Last(); //finds the first element of the queue.
+
+        //        if (currentChar >= 65 && currentChar <= 90 || //if it is an english character.
+        //           (currentChar >= 97 && currentChar <= 122))
+        //        {
+        //            //mainForm.plainTextQueue.Dequeue();
+        //            //ExtendCurrentToken();
+        //            //ToDigitDotState();
+        //            mainForm.plainTextQueue.Dequeue();
+        //            ExtendCurrentToken();
+        //        }
+        //        else
+        //        {
+        //            WriteCurrentTokenInList();
+        //            ToInitialState();
+        //        }
+        //    }
+        //    else
+        //    {
+        //        if (mainForm.currentToken != null)
+        //        {
+        //            WriteCurrentTokenInList();
+        //        }
+
+        //        mainForm.isFinished = true; // Stops the Main While of program.
+        //    }
+        //}
+
+        private void WriteCurrentTokenInList()
+        {
+            mainForm.tokensList.Add(new Token(mainForm.currentToken, "Special Token"));
+            mainForm.currentToken = null;
+        }
+
+        //private void ExtendCurrentToken()
+        //{
+        //    int ascii = Convert.ToInt32(mainForm.tokensLinkedList.First());
+
+        //    if (!(ascii > 0 && ascii <= 32))
+        //    {
+        //        mainForm.currentToken += mainForm.tokensLinkedList.First();
+        //    }
+
+        //    mainForm.tokensLinkedList.RemoveFirst();
+
+        //    //if (mainForm.tokensLinkedList.Count == 0)
+        //    //{
+        //    //    WriteCurrentTokenInList();
+        //    //}
+        //}
     }
 }
