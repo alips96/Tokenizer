434d2cc99c84b4de5082bd5457accfc8b08102be
alips96 <alips96@gmail.com>
2020-04-23 10:56:43 +0200

    real number state

--- RealNumber.cs
+++ RealNumber.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace TokenizerProject
 {
-    public class RealNumber : IState
+    public class RealNumber : IGetBackToInitialState
     {
         MainForm mainForm;
         public RealNumber(MainForm _mainForm)
@@ -14,9 +14,77 @@ namespace TokenizerProject
             mainForm = _mainForm;
         }
 
+        public void ToInitialState()
+        {
+            mainForm.currentState = mainForm.initialState;
+        }
+
+        public void ToRealNumberDotState()
+        {
+            mainForm.currentState = mainForm.realNumberDotState;
+        }
+
         public void UpdateState()
         {
-            throw new NotImplementedException();
+            CheckForInput();
+        }
+
+        private void CheckForInput()
+        {
+            if (mainForm.plainTextQueue.Count > 0)
+            {
+                int currentChar = mainForm.plainTextQueue.Last(); //finds the first element of queue.
+
+                if (currentChar == 46) //if it is a '.'
+                {
+                    mainForm.plainTextQueue.Dequeue();
+                    ExtendCurrentToken();
+                    ToRealNumberDotState();
+                }
+                else
+                    if (currentChar >= 48 && currentChar <= 57) //if it is a digit again.
+                {
+                    mainForm.plainTextQueue.Dequeue();
+                    ExtendCurrentToken();
+                }
+                else
+                {
+                    WriteCurrentTokenInList();
+                    ToInitialState();
+                }
+            }
+            else
+            {
+                if (mainForm.currentToken != null)
+                {
+                    WriteCurrentTokenInList();
+                }
+
+                mainForm.isFinished = true; // Stops the Main While of program.
+            }
+        }
+
+        private void WriteCurrentTokenInList()
+        {
+            mainForm.tokensList.Add(new Token(mainForm.currentToken, "Real Number Token"));
+            mainForm.currentToken = null;
+        }
+
+        private void ExtendCurrentToken()
+        {
+            int ascii = Convert.ToInt32(mainForm.tokensLinkedList.First());
+
+            if (!(ascii > 0 && ascii <= 32))
+            {
+                mainForm.currentToken += mainForm.tokensLinkedList.First();
+            }
+
+            mainForm.tokensLinkedList.RemoveFirst();
+
+            //if (mainForm.tokensLinkedList.Count == 0)
+            //{
+            //    WriteCurrentTokenInList();
+            //}
         }
     }
 }
